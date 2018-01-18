4dac29fa1c02ec86fdf8213973412fb27308a8a0
Greg Look <greg@greg-look.net>
2018-01-17 16:07:19 -0800

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,15 @@
   :url "http://github.com/riemann/riemann-jvm-profiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [cheshire "5.3.1"]
-                 [interval-metrics "1.0.0"]
-                 [clj-radix "0.1.0"]
-                 [http-kit "2.1.16"]
-                 [clj-time "0.7.0"]]
+
+  :dependencies
+  [[org.clojure/clojure "1.9.0"]
+   [org.clojure/tools.logging "0.4.0"]
+   [cheshire "5.8.0"]
+   [interval-metrics "1.0.0"]
+   [clj-radix "0.1.0"]
+   [http-kit "2.2.0"]
+   [clj-time "0.14.2"]]
+
   :java-source-paths ["src/riemann"]
   :manifest {"premain-Class" "riemann.jvm_profiler.Agent"})
